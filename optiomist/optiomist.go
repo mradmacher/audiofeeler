fb4d218d5e23b2f40cf4ccdc80f7b2ffbddc1b64
mradmacher <michal@radmacher.pl>
2024-02-26 16:46:31 +0000

    add some docs for optiomist

--- optiomist/optiomist.go
+++ optiomist/optiomist.go
@@ -1,51 +1,75 @@
 package optiomist
 
+// Describes status of an option.
 type OptionStatus int
+
+// Option has nil value.
 const OptionNil = -1
+// Option is undefined.
 const OptionNone = 0
+// Option has some value.
 const OptionSome = 1
 
 type Option[T any] struct {
-    value T
-    status OptionStatus
+	value  T
+	status OptionStatus
 }
 
+// Defines behaviour of an Option.
 type Optionable interface {
-    IsSome() bool
-    IsNone() bool
-    IsNil() bool
-    Value() any
+	IsSome() bool
+	IsNone() bool
+	IsNil() bool
+	Value() any
 }
 
-func Optiomize[T any](value T, valid bool) Option[T] {
-    if valid {
-        return Some[T](value)
-    } else {
-        return None[T]()
-    }
+// It converts provided value of type T to an Option[T].
+// If the second parameter is true, it returns a Some Option.
+// Otherwise it returns None Option.
+func Optiomize[T any](value T, some bool) Option[T] {
+	if some {
+		return Some[T](value)
+	} else {
+		return None[T]()
+	}
 }
 
+// Creates an option with a value of type T.
 func Some[T any](value T) Option[T] {
-    return Option[T]{value, OptionSome}
+	return Option[T]{value, OptionSome}
 }
+
+// Creates an option with an undefined value of type T.
 func None[T any]() Option[T] {
-    return Option[T]{ status: OptionNone }
+	return Option[T]{status: OptionNone}
 }
+
+// Creates an option with a nil value of type T.
 func Nil[T any]() Option[T] {
-    return Option[T]{ status: OptionNil }
+	return Option[T]{status: OptionNil}
 }
+
+// Does the option have some value?
 func (opt Option[T]) IsSome() bool {
-    return opt.status == OptionSome || opt.status == OptionNil
+	return opt.status == OptionSome || opt.status == OptionNil
 }
+
+// Is the option undefined?
 func (opt Option[T]) IsNone() bool {
-    return opt.status == OptionNone
+	return opt.status == OptionNone
 }
+
+// Does the option have nil value?
 func (opt Option[T]) IsNil() bool {
-    return opt.status == OptionNil
+	return opt.status == OptionNil
 }
+
+// Returns the value of an option without specified type.
 func (opt Option[T]) Value() any {
-    return opt.value
+	return opt.value
 }
+
+// Returns the value of an option with the option's type.
 func (opt Option[T]) TypedValue() T {
-    return opt.value
+	return opt.value
 }
--- optiomist/optiomist_test.go
+++ optiomist/optiomist_test.go
@@ -1,61 +1,102 @@
 package optiomist
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestSomeString(t *testing.T) {
-    value := "I'm here"
-    opt := Some(value)
-    if !opt.IsSome() {
-        t.Errorf("Some.IsSome() = false; expected true")
-    }
+func Example() {
+	type Test struct {
+		V1 Option[string]
+		V2 Option[string]
+		V3 Option[string]
+	}
 
-    if opt.IsNone() {
-        t.Errorf("Some.IsNone() = true; expected false")
-    }
+	test := Test{
+		Some("I'm a string"),
+		None[string](),
+		Nil[string](),
+	}
 
-    if opt.IsNil() {
-        t.Errorf("Some.IsNil() = true; expected false")
-    }
+	fmt.Printf("Some[some: %v, none: %v, nil: %v]: %q\n",
+		test.V1.IsSome(),
+		test.V1.IsNone(),
+		test.V1.IsNil(),
+		test.V1.Value())
 
-    if opt.Value() != value {
-        t.Errorf("Some.Value() = %q; expected %q", opt.Value(), value)
-    }
+	fmt.Printf("None[some: %v, none: %v, nil: %v]: %q\n",
+		test.V2.IsSome(),
+		test.V2.IsNone(),
+		test.V2.IsNil(),
+		test.V2.Value())
+
+	fmt.Printf("Nil[some: %v, none: %v, nil: %v]: %q\n",
+		test.V3.IsSome(),
+		test.V3.IsNone(),
+		test.V3.IsNil(),
+		test.V3.Value())
+
+	// Output:
+	// Some[some: true, none: false, nil: false]: "I'm a string"
+	// None[some: false, none: true, nil: false]: ""
+	// Nil[some: true, none: false, nil: true]: ""
 }
 
-func TestNoneString(t *testing.T) {
-    opt := None[string]()
-    if opt.IsSome() {
-        t.Errorf("Some.IsSome() = true; expected false")
-    }
+func TestSome_string(t *testing.T) {
+	value := "I'm here"
+	opt := Some(value)
+	if !opt.IsSome() {
+		t.Error("Some.IsSome() = false; expected true")
+	}
 
-    if !opt.IsNone() {
-        t.Errorf("Some.IsNone() = false; expected true")
-    }
+	if opt.IsNone() {
+		t.Error("Some.IsNone() = true; expected false")
+	}
 
-    if opt.IsNil() {
-        t.Errorf("Some.IsNil() = true; expected false")
-    }
+	if opt.IsNil() {
+		t.Error("Some.IsNil() = true; expected false")
+	}
 
-    if opt.Value() != nil {
-        t.Errorf("Some.Value() = %v; expected %v", opt.Value(), nil)
-    }
+	if opt.Value() != value {
+		t.Errorf("Some.Value() = %q; expected %q", opt.Value(), value)
+	}
+
+	if opt.TypedValue() != value {
+		t.Errorf("Some.TypedValue() = %q; expected %q", opt.TypedValue(), value)
+	}
 }
 
-func TestNilString(t *testing.T) {
-    opt := Nil[string]()
-    if !opt.IsSome() {
-        t.Errorf("Some.IsSome() = false; expected true")
-    }
+func TestNone_string(t *testing.T) {
+	opt := None[string]()
+	if opt.IsSome() {
+		t.Error("Some.IsSome() = true; expected false")
+	}
 
-    if opt.IsNone() {
-        t.Errorf("Some.IsNone() = true; expected false")
-    }
+	if !opt.IsNone() {
+		t.Error("Some.IsNone() = false; expected true")
+	}
 
-    if !opt.IsNil() {
-        t.Errorf("Some.IsNil() = false; expected true")
-    }
+	if opt.IsNil() {
+		t.Error("Some.IsNil() = true; expected false")
+	}
+}
 
-    if opt.Value() != nil {
-        t.Errorf("Some.Value() = %q; expected %q", opt.Value(), "")
-    }
+func TestNil_string(t *testing.T) {
+	opt := Nil[string]()
+	if !opt.IsSome() {
+		t.Error("Some.IsSome() = false; expected true")
+	}
+
+	if opt.IsNone() {
+		t.Error("Some.IsNone() = true; expected false")
+	}
+
+	if !opt.IsNil() {
+		t.Error("Some.IsNil() = false; expected true")
+	}
+
+	var want string // nil string
+	if opt.Value() != want {
+		t.Errorf("Some.Value() = %q; expected nil", opt.Value())
+	}
 }
