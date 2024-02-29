36e47628fb8f3f392232341d3603546c8c591dec
mradmacher <michal@radmacher.pl>
2024-02-29 11:42:17 +0000

    add dbseed script

--- cmd/playground/play.go
+++ cmd/playground/play.go
@@ -3,10 +3,7 @@ package main
 import (
     "fmt"
     "os"
-    "time"
     "github.com/mradmacher/audiofeeler/internal/repo"
-    "github.com/mradmacher/audiofeeler/optiomist"
-    "github.com/mradmacher/audiofeeler/internal"
 )
 
 func main() {
@@ -16,54 +13,18 @@ func main() {
     }
     defer db.Close()
 
+    //defer func() {
+    //    db.RemoveStructure()
+    //    fmt.Println("Tables dropped")
+    //}()
+
     r := repo.EventsRepo { db }
-
-    if db.Ping() {
-        fmt.Println("Connected")
-    } else {
-        fmt.Println("Not connected")
-    }
-
-	err = db.CreateStructure()
-    if err != nil {
-        panic(err)
-    }
-
-    defer func() {
-        db.RemoveStructure()
-        fmt.Println("Tables dropped")
-    }()
-
-    fmt.Println("Tables created")
-
-    params := audiofeeler.Event {
-        Date: optiomist.Some(time.Date(2024, 2, 1, 0, 0, 0, 0, time.UTC)),
-        Hour: optiomist.Nil[time.Time](),
-        Venue: optiomist.Some("Some venue"),
-        Address: optiomist.Some("Some address"),
-        Town: optiomist.Some("Some town"),
-
-    }
-    id, err := r.Create(params)
-    fmt.Printf("Event created [%v]: %v\n", id, err)
-
-    params = audiofeeler.Event {
-        Date: optiomist.Some(time.Date(2024, 3, 1, 0, 0, 0, 0, time.UTC)),
-        Hour: optiomist.Some(time.Date(0, 0, 0, 21, 0, 0, 0, time.UTC)),
-        Venue: optiomist.Some("Other venue"),
-        Address: optiomist.Some("Other address"),
-        Town: optiomist.None[string](),
-
-    }
-    id, err = r.Create(params)
-    fmt.Printf("Event created [%v]: %v\n", id, err)
-
     events, err := r.All()
     if err != nil {
         panic(err)
     }
 
     for _, event := range *events {
-        fmt.Printf("Event: %v\n", event)
+        fmt.Printf("Event[%v]: %v\n", event.Date.Value(), event)
     }
 }
