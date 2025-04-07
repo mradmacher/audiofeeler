7a86c18bf88e80e42a237486e7b7be759ea6d910
mradmacher <michal@radmacher.pl>
2025-04-08 00:03:10 +0200

    encode username and password

--- spec/deploy_inventory_spec.cr
+++ spec/deploy_inventory_spec.cr
@@ -2,12 +2,51 @@ require "./spec_helper"
 
 describe "DeployInventory" do
   account_inventory = Audiofeeler::AccountInventory.new(TESTDB)
+  deploy_inventory = Audiofeeler::DeployInventory.new(TESTDB)
 
   account = account_inventory.find_one(
     account_inventory.create({"name" => "Test"}).unwrap
   ).unwrap
 
-  it "works" do
-    account.name.should eq("Test")
+  describe "#create" do
+    it "creates new deploy" do
+      result = deploy_inventory.create(account.id, {
+        "server" => "example.com",
+        "local_dir" => "here",
+        "remote_dir" => "there",
+      })
+      result.ok?.should be_true
+
+      result = deploy_inventory.find_one(result.unwrap)
+      result.ok?.should be_true
+      deploy = result.unwrap
+
+      deploy.server.should eq "example.com"
+      deploy.local_dir.should eq "here"
+      deploy.remote_dir.should eq "there"
+      deploy.username.should be_nil
+      deploy.password.should be_nil
+    end
+
+    it "encrypts username and password" do
+      plain_username = "look at me"
+      plain_password = "you can see me"
+      result = deploy_inventory.create(account.id, {
+        "server" => "example.com",
+        "username" => plain_username,
+        "password" => plain_password,
+      })
+      result.ok?.should be_true
+
+      result = deploy_inventory.find_one(result.unwrap)
+      result.ok?.should be_true
+      deploy = result.unwrap
+
+      deploy.username.should_not be_nil
+      deploy.username.should_not eq plain_username
+
+      deploy.password.should_not be_nil
+      deploy.password.should_not eq plain_password
+    end
   end
 end
--- src/audiofeeler/deploy_inventory.cr
+++ src/audiofeeler/deploy_inventory.cr
@@ -1,9 +1,31 @@
 require "result"
+require "openssl"
+require "random/secure"
 
 module Audiofeeler
   class DeployInventory
     def initialize(@db : DB::Database)
       @db = db
+    end
+
+    def find_one(id)
+      @db.query_one "SELECT id, account_id, server, local_dir, remote_dir, username, password FROM deploys WHERE id = ?", id do |rs|
+        return Ok.done(
+          Deploy.new(
+            id: rs.read(Int64),
+            account_id: rs.read(Int64),
+            server: rs.read(String),
+            local_dir: rs.read(String?),
+            remote_dir: rs.read(String?),
+            username: rs.read(String?),
+            password: rs.read(String?),
+          )
+        )
+      end
+    rescue ex: DB::NoResultsError
+      Err.not_found(ex)
+    rescue ex: DB::Error
+      Err.fail(ex)
     end
 
     def find_all(account_id)
@@ -26,12 +48,41 @@ module Audiofeeler
     end
 
     def create(account_id, params)
-      result = @db.exec "INSERT INTO deploys (account_id, server, local_dir, remote_dir, username, password) VALUES (?, ?, ?, ?, ?, ?)",
-        account_id, params["deployl[server]"], params["deploy[local_dir]"], params["deploy[remote_dir]"], params["deploy[username]"], params["deploy[password]"]
+      key = Random::Secure.random_bytes(64)
+      username = params["username"]?
+      password = params["password"]?
+      username_iv = username ? Random::Secure.random_bytes(32) : nil
+      password_iv = password ? Random::Secure.random_bytes(32) : nil
+
+      result = @db.exec "INSERT INTO deploys (account_id, server, local_dir, remote_dir, username, username_iv, password, password_iv) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+        account_id,
+        params["server"]?,
+        params["local_dir"]?,
+        params["remote_dir"]?,
+        encrypt(username, key, username_iv),
+        username_iv,
+        encrypt(password, key, password_iv),
+        password_iv
 
       Ok.created(result.last_insert_id)
     rescue ex: DB::Error
       Err.fail(ex)
+    end
+
+    private def encrypt(data, key, iv)
+      return nil if data.nil?
+
+      cipher = OpenSSL::Cipher.new("aes-256-cbc")
+      cipher.encrypt
+      cipher.key = key
+      cipher.random_iv
+
+      io = IO::Memory.new
+      io.write(cipher.update(data))
+      io.write(cipher.final)
+      io.rewind
+
+      Base64.encode(io.to_s)
     end
   end
 end
