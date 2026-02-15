4d9c17020590123c35e0d52d299634f944a2151f
mradmacher <michal@radmacher.pl>
2026-02-15 10:33:01 +0100

    add editing events

--- internal/events_controller.go
+++ internal/events_controller.go
@@ -17,7 +17,7 @@ type EventsPresenter struct {
 type EventsController struct {
 	app           *App
 	indexTemplate Template
-	newTemplate   Template
+	formTemplate   Template
 }
 
 func NewEventsController(app *App) *EventsController {
@@ -28,7 +28,7 @@ func NewEventsController(app *App) *EventsController {
 	eventRepo := EventRepo{app.db}
 
 	controller.indexTemplate = app.templateEngine.MustParse("events", "account_wrapper")
-	controller.newTemplate = app.templateEngine.MustParse("event_form", "account_wrapper")
+	controller.formTemplate = app.templateEngine.MustParse("event_form", "account_wrapper")
 
 	app.router.HandleFunc("GET /accounts/{accountName}/events/{$}", func(w http.ResponseWriter, r *http.Request) {
 		accountName := r.PathValue("accountName")
@@ -54,9 +54,32 @@ func NewEventsController(app *App) *EventsController {
 			panic(err)
 		}
 		if accountFindResult.IsFound {
-			err = controller.newTemplate.Execute(w, r, EventPresenter{accountFindResult.Record, Event{}})
+			err = controller.formTemplate.Execute(w, r, EventPresenter{accountFindResult.Record, Event{}})
 			if err != nil {
 				panic(err)
+			}
+		}
+	})
+
+	app.router.HandleFunc("GET /accounts/{accountName}/events/{eventId}/edit", func(w http.ResponseWriter, r *http.Request) {
+		accountName := r.PathValue("accountName")
+		accountFindResult, err := accountRepo.FindByName(accountName)
+		if err != nil {
+			panic(err)
+		}
+		if accountFindResult.IsFound {
+			account := accountFindResult.Record
+			eventId := DatabaseId(r.PathValue("eventId"))
+		    eventFindResult, err := eventRepo.Find(eventId)
+			if err != nil {
+				panic(err)
+			}
+			if eventFindResult.IsFound {
+				event := eventFindResult.Record
+				err = controller.formTemplate.Execute(w, r, EventPresenter{account, event})
+				if err != nil {
+					panic(err)
+				}
 			}
 		}
 	})
@@ -81,7 +104,39 @@ func NewEventsController(app *App) *EventsController {
 			event.AccountId = account.Id
 			eventRepo.Save(event)
 
-			http.Redirect(w, r, "/accounts/"+accountName+"/events", http.StatusFound)
+			http.Redirect(w, r, "/accounts/"+accountName+"/events", http.StatusSeeOther)
+		}
+	})
+
+	app.router.HandleFunc("PUT /accounts/{accountName}/events/{eventId}", func(w http.ResponseWriter, r *http.Request) {
+		accountName := r.PathValue("accountName")
+		accountFindResult, err := accountRepo.FindByName(accountName)
+		if err != nil {
+			panic(err)
+		}
+		if accountFindResult.IsFound {
+			eventId := DatabaseId(r.PathValue("eventId"))
+		    eventFindResult, err := eventRepo.Find(eventId)
+			if err != nil {
+				panic(err)
+			}
+			if eventFindResult.IsFound {
+				event := eventFindResult.Record
+				event.Name = r.PostFormValue("event[name]")
+				event.Date = r.PostFormValue("event[date]")
+				event.Hour = r.PostFormValue("event[hour]")
+				event.Venue = r.PostFormValue("event[venue]")
+				event.Town = r.PostFormValue("event[town]")
+				event.Location = r.PostFormValue("event[location]")
+				event.Description = r.PostFormValue("event[description]")
+				_, err = eventRepo.Save(event)
+
+				if err != nil {
+					panic(err)
+				}
+
+				http.Redirect(w, r, "/accounts/"+accountName+"/events", http.StatusSeeOther)
+			}
 		}
 	})
 
--- src/web/views/videos.ecr
+++
@@ -1,1 +0,0 @@
-No videos yet
